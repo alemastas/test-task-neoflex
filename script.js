f21da6e58008b890c01126d45683fb0122ff1019
alemastas <mastauxus@yandex.ru>
2023-04-26 16:42:54 +0400

    init: create a new project

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+window.onload = function(){
+
+    const buttonEng = document.getElementById('eng');
+    const buttonRus = document.getElementById('rus');
+
+    buttonRus.addEventListener('click', function(){
+        eng = false;
+        console.log('lang is rus')
+    })
+
+    buttonEng.addEventListener('click', function(){
+        eng = true;
+        console.log('lang is eng')
+    })
+}
+
